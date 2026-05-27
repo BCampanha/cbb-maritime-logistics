c7358aa0465ee410c03f219835c9b2e1bbc08372
Beatriz Campanha Silva <bcampanha@alumni.usp.br>
2026-05-27 12:55:48 -0300

    Toggle de abas

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -40,4 +40,22 @@ async function carregarDashboard() {
     `;
 }
 
+function ver(id) {
+    const financeiro = document.getElementById("financeiro-especifico");
+    const financeiroAba = document.getElementById("financeiro-aba");
+    const operacional = document.getElementById("operacional-especifico");
+    const operacionalAba = document.getElementById("operacional-aba");
+    const atual = document.getElementById(id+'-especifico');
+    const atualAba = document.getElementById(id+'-aba');
+    console.log(id)
+    if (atual.style.display === 'none') {
+        operacional.style.display = 'none'; 
+        financeiro.style.display = 'none';
+        financeiroAba.style.backgroundColor = '#84C2DD';
+        operacionalAba.style.backgroundColor = '#84C2DD';
+        atual.style.display = 'flex';
+        atualAba.style.backgroundColor = '#B0D8E9';
+    }
+}
+
 carregarDashboard();
